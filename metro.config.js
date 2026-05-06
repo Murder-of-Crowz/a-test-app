68c3738552df6c4efdd62e2d4f4d2d3d72a56039
Paul De Silos <pauldesilos@yahoo.com>
2026-05-05 23:24:02 -0500

    chore: add "version" to asset extensions in metro configuration

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,8 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.assetExts.push("version");
+
 config.resolver.resolveRequest = (context, moduleName, platform) => {
   if (moduleName === "better-sqlite3") {
     return { type: "empty" }
