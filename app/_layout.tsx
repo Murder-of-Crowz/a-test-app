8c439db737a281c807b7cf4ddcd7595cc95de85f
Paul De Silos <pauldesilos@yahoo.com>
2026-05-05 23:24:50 -0500

    removed firebase imports and commented out code. Added preloaded function for premQ.db questions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,19 @@
-import { Stack, useRouter, useSegments } from "expo-router";
-import { useEffect, useState } from "react";
-// import { onAuthStateChanged, User } from "firebase/auth";
-// import { auth } from "@/path/to/firebase"
+import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+// @ts-ignore
+import { initPremDB, getPremQuestions } from "@/src/premDB";
 
 export default function RootLayout() {
-  // const [user, setUser] = useState<User | null>(null);
-  // const [ready, setReady] = useState(false);
-  // const router = useRouter();
-  // const segments = useSegments();
+  useEffect(() => {
+    initPremDB()
+      .then(() => console.log("premDB ready:", getPremQuestions().length, "questions"))
+      .catch(console.error);
+    }, []);
 
-  // useEffect(() => {
-  //   return onAuthStateChanged(AuthenticatorAssertionResponse, (u) => {
-  //     setUser(u);
-  //     setReady(true);
-  //   });
-  // }, []);
-
-  // useEffect(() => {
-  //   if (!ready) return;
-
-  //   const onAuthScreen = !segments[0] || segments[0] === "login";
-    
-  //   if (!user && !onAuthScreen) router.replace("/login");
-  //   if (user && onAuthScreen) router.replace("/dashboard");
-  //   }, [user, ready, segments])
-
-  //   if (!ready) return null;
-
-  return <Stack screenOptions={{ headerShown: false }}/>;
+  return (
+  <GestureHandlerRootView style={{ flex: 1 }}>
+    <Stack screenOptions={{ headerShown: false }}/>
+  </GestureHandlerRootView>
+  )
 }
