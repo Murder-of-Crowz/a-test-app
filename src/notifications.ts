eb52c0a0761c10e114cf9b3d315b403ff8334f71
Paul De Silos <pauldesilos@yahoo.com>
2026-05-13 22:24:55 -0500

    refactor: move ensureChannel function and update notification scheduling logic (1s til pop-up for test)

--- src/notifications.ts
+++ src/notifications.ts
@@ -1,13 +1,17 @@
 import * as Notifications from "expo-notifications";
 import { Platform } from "react-native";
 
-export async function requestPermission(): Promise<boolean> {
+async function ensureChannel() {
   if (Platform.OS === "android") {
     await Notifications.setNotificationChannelAsync("default", {
       name: "Default",
       importance: Notifications.AndroidImportance.MAX,
     });
-  }
+  }  
+}
+
+export async function requestPermission(): Promise<boolean> {
+  await ensureChannel();
   const { status } = await Notifications.requestPermissionsAsync();
   return status === "granted";
 }
@@ -18,11 +22,14 @@ export async function getPermissionStatus() {
 }
 
 export async function scheduleReminder(hour: number, minute: number): Promise<void> {
+  await ensureChannel();
   await Notifications.cancelAllScheduledNotificationsAsync();
   await Notifications.scheduleNotificationAsync({
     content: {
       title: "Time to Study!",
       body: "Don't slack off now, you got this!",
+      sound: true,
+      ...(Platform.OS === "android" && { channelId: "default" }),
     },
     trigger: {
       type: Notifications.SchedulableTriggerInputTypes.DAILY,
@@ -33,14 +40,17 @@ export async function scheduleReminder(hour: number, minute: number): Promise<vo
 }
 
 export async function sendTestNotif(): Promise<void> {
+  await ensureChannel();
   await Notifications.scheduleNotificationAsync({
     content: {
       title: "Test",
       body: "Text is good!",
+      sound: true,
+      ...(Platform.OS === "android" && { channelId: "default" })
     },
     trigger: {
       type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
-      seconds: 5,
+      seconds: 1,
     },
   });
 }
